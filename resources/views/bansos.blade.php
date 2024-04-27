913fbb33dabb4c428394096dc1be9e43de171bf9
akfalah <akfalah1107@gmail.com>
2024-04-27 23:49:09 +0700

    update: fixing slicing bansos page

--- resources/views/bansos.blade.php
+++ resources/views/bansos.blade.php
@@ -2,40 +2,50 @@
 
 @section('content')
     <main class="p-2">
-        <div class="outerCard items-center">
-            <section class="innerCard mt-20 w-[552]">
-                <div class="text-center mb-8">
-                    <p class="cardTitle">Cek Data Anda</p>
-                    <p class="subtitle text-Neutral-40">Cek data penerima bansos di Dusun Gondorejo.</p>
-                </div>
-
-                <form action="#" method="post">
-                    <div class="mb-4">
-                        <label class="subtitle" for="nik">NIK</label>
-                        <input type="text" name="nik" id="nik" placeholder="352xxxxxxxxxxxxx">
+        <section class="outerCheckData">
+            <form action="#" method="post" class="innerCheckData">
+                <div class="flex flex-col gap-8">
+                    <div class="flex flex-col gap-2 text-center">
+                        <p class="cardTitle">Cek Data Anda</p>
+                        <p class=" text-Neutral-40">Cek data penerima bansos di Dusun Gondorejo.</p>
                     </div>
                     
-                    <div>
-                        <p class="subtitle">Tanggal Lahir</p>
-                        <div class="mb-3">
-                            <select name="tanggal" id="tanggal">Tangal
-                                <option value="" class=" text-Neutral-40" >Tanggal</option>
-                            </select>
-                            <select name="bulan" id="bulan">
-                                <option value="" class=" text-Neutral-40" >Bulan</option>
-                            </select>
-                            <select name="tahun" id="tahun">
-                                <option value="" class=" text-Neutral-40" >Tahun</option>
-                            </select>
+                    <div class="flex flex-col gap-4">
+                        <div class="flex flex-col gap-1">
+                            <label class="subtitle" for="nik">NIK</label>
+                            <input type="text" name="nik" id="nik" placeholder="352xxxxxxxxxxxxx">
+                        </div>
+
+                        <div class="flex flex-col gap-1">
+                            <label class="subtitle">Tanggal Lahir</label>
+                            <div class="flex gap-2">
+                                <select name="tanggal" id="tanggal">
+                                    <option value="" class=" text-Neutral-40" >Tanggal</option>
+                                    @for ($i = 1; $i <= 31; $i++)
+                                        <option value="{{ $i }}" class=" text-Neutral-40" >{{ $i }}</option>
+                                    @endfor
+                                </select>
+                                <select name="bulan" id="bulan">
+                                    <option value="" class=" text-Neutral-40" >Bulan</option>
+                                    @for ($i = 1; $i <= 12; $i++)
+                                        <option value="{{ $i }}" class=" text-Neutral-40" >{{ $i }}</option>
+                                    @endfor
+                                </select>
+                                <select name="tahun" id="tahun">
+                                    <option value="" class=" text-Neutral-40" >Tahun</option>
+                                    @for ($i = 1900; $i <= now()->year; $i++)
+                                        <option value="{{ $i }}" class=" text-Neutral-40" >{{ $i }}</option>
+                                    @endfor
+                                </select>
+                            </div>
                         </div>
                     </div>
-
-                    <div class="flex justify-end mt-12">
-                        <a href="#" class="buttonLight mr-2">Ajukan Data</a>
-                        <button type="submit" class="buttonDark">Cek Data</button>
-                    </div>
-                </form>
-            </section>
-        </div>
+                </div>
+                <div class="flex gap-2 justify-end">
+                    <a href="#" class="buttonLight">Ajukan Data</a>
+                    <button type="submit" class="buttonDark">Cek Data</button>
+                </div>
+            </form>
+        </section>
     </main>
 @endsection
