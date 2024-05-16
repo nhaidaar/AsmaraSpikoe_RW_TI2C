4ca5df93adbee33fda5e7e819549c687dea4398e
Naufal Haidar <naufalsetyawan015@gmail.com>
2024-05-16 23:05:32 +0700

    update: implement rt page

--- app/Http/Controllers/RtController.php
+++ app/Http/Controllers/RtController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\RTModel;
 use Illuminate\Http\Request;
 
 class RtController extends Controller
@@ -10,8 +11,11 @@ class RtController extends Controller
 
     public function index()
     {
+        $rt = RTModel::with('ketuaRT')->get();
+
         return view('rt.index', [
             'active' => $this->active,
+            'rt' => $rt
         ]);
     }
 }
