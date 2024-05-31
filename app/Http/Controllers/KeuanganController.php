db557ea2e55bbee310839c284fffa8087b3973d3
Naufal Haidar <naufalsetyawan015@gmail.com>
2024-05-31 09:02:47 +0700

    update: improving ui keuangan page

--- app/Http/Controllers/KeuanganController.php
+++ app/Http/Controllers/KeuanganController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KeuanganModel;
 use App\Models\RTModel;
+use App\Models\WargaModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,27 +14,28 @@ class KeuanganController extends Controller
 
     public function index()
     {
+        $active = $this->active;
+
         // Default rt is 1
         $rt = 1;
 
         // If user not ketua rw, choose their rt
-        $user = Auth::user();
-        if ($user->level != 'rw') {
-            $rt = RTModel::whereHas('kartuKeluarga.detailKK.anggotaKeluarga', function ($q) use ($user) {
-                $q->where('warga_id', $user->warga_id);
-            })
-                ->pluck('rt_id')
-                ->first();
+        if (Auth::check() && Auth::user()->level != 'rw') {
+            $user = WargaModel::with('detailKK.kartuKeluarga')->find(Auth::user()->warga_id);
+            $rt = $user->detailKK->kartuKeluarga->rt;
         }
 
-        return view('keuangan.index', [
-            'active' => 'keuangan', 
-            'rt' => $rt,
-        ]);
+        $keuangan = KeuanganModel::all();
+        $totalPemasukan = KeuanganModel::where('jenis_keuangan', 'Pemasukkan')->sum('nominal');
+        $totalPengeluaran = KeuanganModel::where('jenis_keuangan', 'Pengeluaran')->sum('nominal');
+        $totalKas = $totalPemasukan - $totalPengeluaran;
+
+        return view('keuangan.index', compact('active', 'rt', 'keuangan', 'totalPengeluaran', 'totalKas'));
     }
 
-    public function create(){
-        return view ('keuangan.create_transaksi', [
+    public function create()
+    {
+        return view('keuangan.create', [
             'active' => $this->active
         ]);
     }
