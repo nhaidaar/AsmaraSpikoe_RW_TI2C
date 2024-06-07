afc36652b7c5ba1e19598bf0da813804abb35133
Naufal Haidar <naufalsetyawan015@gmail.com>
2024-06-07 23:28:51 +0700

    update: change file name format for persuratan

--- resources/views/persuratan/child.blade.php
+++ resources/views/persuratan/child.blade.php
@@ -18,7 +18,7 @@ Carbon::setLocale('id');
         <td>{{ $item->surat_jenis }}</td>
         <td>{{ $item->surat_tujuan }}</td>
         <td class="flex gap-2 max-w-20">
-            <a href="{{ url('surat/' . $item->surat_id . '.docx') }}" class="bg-Primary-Base text-Neutral-0 font-medium px-4 py-2.5 gap-1 rounded-lg">Unduh</a>
+            <a href="{{ url('surat/' . '[' . Carbon::parse($item->surat_tanggal)->translatedFormat('j F Y') . '] - ' . $item->pengajuSurat->nama_warga . '_'  . substr($item->surat_id, -5) . '.docx') }}" class="bg-Primary-Base text-Neutral-0 font-medium px-4 py-2.5 gap-1 rounded-lg">Unduh</a>
         </td>
     </tr>
 @endforeach
