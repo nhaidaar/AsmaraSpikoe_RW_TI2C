54ebf75a84f1ae5585feaf87fb6a530ef89ee8c1
akfalah <akfalah1107@gmail.com>
2024-06-11 22:28:22 +0700

    update: seed keuangan

--- database/seeders/KeuanganSeeder.php
+++ database/seeders/KeuanganSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Faker\Factory as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,24 +14,32 @@ class KeuanganSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = 
-        [
-            [
-                'rt_id'                 => '1',
-                'tanggal'               => now(),
-                'jenis_keuangan'        => 'Pemasukkan',
-                'nominal'               => 1000000,
-                'keterangan_keuangan'   => 'Iuran Sampah'
-            ],
-            [
-                'rt_id'                 => '1',
-                'tanggal'               => now(),
-                'jenis_keuangan'        => 'Pengeluaran',
-                'nominal'               => 100000,
-                'keterangan_keuangan'   => 'Pembelian Kain Kafan'
-            ],
-        ];
+        $faker = Faker::create('id_ID');
+        $startDate = '2024-01-01';
+        $endDate = '2024-06-30';
 
-        DB::table('keuangan')->insert($data);
+        $rt = 1;
+        for ($i = 1; $i <= 25; $i++) {
+            $jenisKeuangan = $faker->randomElement(['Pemasukkan', 'Pengeluaran']);
+
+            if ($jenisKeuangan === 'Pemasukkan') {
+                $keteranganKeuangan = $faker->randomElement(['Kas rutin', 'Iuran sampah', 'Iuran kematian']);
+            } else {
+                $keteranganKeuangan = $faker->randomElement(['Pembayaran sampah', 'Pembelian kain kafan']);
+            }
+
+            DB::table('keuangan')->insert([
+                'rt_id'                 => $rt,
+                'tanggal'               => $faker->dateTimeBetween($startDate, $endDate),
+                'jenis_keuangan'        => $jenisKeuangan,
+                'nominal'               => $faker->randomElement([500000, 300000, 250000, 200000, 150000]),
+                'keterangan_keuangan'   => $keteranganKeuangan
+            ]);
+
+            $rt++;
+            if ($rt == 8) {
+                $rt = 1;
+            }
+        }
     }
 }
