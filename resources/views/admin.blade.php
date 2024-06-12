5ca8093aa7d7ff66cd59ce336e635256e4b0f51b
akfalah <akfalah1107@gmail.com>
2024-06-12 10:00:55 +0700

    update: view index admin

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -19,14 +19,14 @@
             <p class="cardTitle">Data Persuratan</p>
             <div class="h-[1px] bg-Neutral-20 flex"></div>
             <div class="flex w-full justify-center overflow-auto">
-                <iframe width="900" height="600" src="https://lookerstudio.google.com/embed/reporting/177b3bcc-1588-43db-a542-0117833d4c3f/page/KAv2D" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+                <iframe width="900" height="600" src="https://lookerstudio.google.com/embed/reporting/b8a25dc5-02d3-4727-a077-396434910659/page/X712D"></iframe>
             </div>
         </section>
         <section class="outerCard">
             <p class="cardTitle">Data Keuangan</p>
             <div class="h-[1px] bg-Neutral-20 flex"></div>
             <div class="flex w-full justify-center overflow-auto">
-                <iframe width="900" height="600" src="https://lookerstudio.google.com/embed/reporting/177b3bcc-1588-43db-a542-0117833d4c3f/page/KAv2D" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+                <iframe width="900" height="600" src="https://lookerstudio.google.com/embed/reporting/362261b5-5ee4-4b40-bc7c-83d6c94f70d7/page/Y812D"></iframe>
             </div>
         </section>
     </main>
